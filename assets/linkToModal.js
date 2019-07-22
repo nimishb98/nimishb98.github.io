b0522de809a047b10be0cea9531c409eb4d136cc
Nimish Baldha <nimishb98@gmail.com>
2019-07-22 15:54:37 +0530

    Update linkToModal.js

--- assets/linkToModal.js
+++ assets/linkToModal.js
@@ -45,7 +45,7 @@ function getDocHeight(doc) {
 }
 function setIframeHeight(id) {
     var ifrm = document.getElementById(id);
-    var doc = ifrm.contentWindow.document;
+    var doc = ifrm.contentDocument;
     ifrm.style.visibility = 'hidden';
     ifrm.style.height = "10px"; // reset to minimal height ...
     // IE opt. for bing/msn needs a bit added or scrollbar appears
